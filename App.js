d77f7a0842893264e05fe1924ccf43ecc0683250
mohameddahy dahy <mohameddahy111@gmail.com>
2024-05-03 14:36:23 +0300

    fixied

--- App.js
+++ App.js
@@ -6,22 +6,25 @@ import Home from './screens/Home';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import axios from 'axios';
+import { DataStoreProvider } from './context/DateStor';
 const Stack = createNativeStackNavigator();
 
 axios.defaults.baseURL = 'https://smart-school-app-back.onrender.com';//fel code kollah
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Home}>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <DataStoreProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={Home}>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DataStoreProvider>
   );
 }
 
